532791cf62e1c6bd0718f0a17b0cd9b664d555dc
Parambalamba <parambalamba@gmail.com>
2024-10-03 11:39:09 +0500

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'afea3cb2a54a8731c37b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '35d74c4b0f024aea6cff');
