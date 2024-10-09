48552244b9935ab90376c972296baa31b3a58d75
Parambalamba <parambalamba@gmail.com>
2024-10-09 10:35:45 +0500

    fix bug with display correct poke's type

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f1e26eba1975aeb27c66');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '97c457d591bf008f21fb');
